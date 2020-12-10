2354e5297b08e3f32b3e9498bf08497f47c3bc0a
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-11 01:31:01 +0530

    Rewrote ntq.sh ntrm.sh

--- ntq.sh
+++ ntq.sh
@@ -1,34 +1,24 @@
 #!/bin/sh
 c_id=$(tput setaf 2)
 c_tm=$(tput setaf 3)
-c_qu=$(tput setaf 4)
 c_ur=$(tput setaf 6)
 c_df=$(tput sgr0)
 
-atq | sort -r -k6,6 -k3,3M -k4,4 -k5,5 |
-    while read -r job ; do
-        ur=${job##* }
-        job=${job% *}
-        qu=${job##* }
-        # skip redundant description of running jobs
-        if [ "$qu" = "=" ] ; then
-            lastactive=1
-            continue
-        elif [ "$lastactive" = 1 ] ; then
-            qu="~$qu"
-        else
-            qu=" $qu"
-        fi
-        job=${job% *}
-        tm=${job#*	}
-        id=${job%%	*}
-        info=$(at -c "$id")
-        nt=$(echo "$info" | grep "^NT_MESSAGE=") || continue
-        eval "$nt"
-        if t=$(echo "$info" | awk '$1=="sleep" {print $3; f=1; exit}; END {exit !f}') ; then
-            tm=$(LC_TIME=C date +%c -d "@$t")
-        fi
-        printf "%s\t%s\n%s\n" \
-            "${c_id}${id}" "${c_tm}${tm} ${c_qu}${qu} ${c_ur}${ur}${c_df}" \
-            "message: $NT_MESSAGE"
-    done
+atq | LC_ALL=C sort -r -k6,6 -k3,3M -k4,4 -k5,5 |
+    awk '!x[$1] {x[$1]=1; printf "%s,%s %s %02d %s %s,%s\n",$1,$2,$3,$4,$5,$6,$8}' |
+        while read -r job ; do
+            id=${job%%,*}
+            info=$(at -c "$id")
+            nt=$(echo "$info" | grep "^NT_MESSAGE=") || continue
+            job=${job#*,}
+            ur=${job#*,}
+            if t=$(echo "$info" | awk '$1=="sleep" {print $3; f=1; exit}; END {exit !f}') ; then
+                tm=$(LC_TIME=C date +%c -d "@$t")
+            else
+                tm=${job%,*}
+            fi
+            eval "$nt"
+            printf "%s\t%s\n%s\n" \
+                "${c_id}${id}" "${c_tm}${tm} ${c_ur}${ur}${c_df}" \
+                "message: $NT_MESSAGE"
+        done
--- ntrm.sh
+++ ntrm.sh
@@ -2,14 +2,38 @@
 [ "$#" -eq 0 ] && { echo "Usage: ntrm -a|<id>"; exit ;}
 case $1 in
     -a)
-        printf "Remove all notification alarms (y/N):"
+        printf "Remove all pending notification alarms (y/N):"
         read -r confirm
         [ "$confirm" != y ] && [ "$confirm" != Y ] && exit
-        atq | cut -f1 | while read -r id ; do
-            at -c "$id" | grep -q "^NT_MESSAGE=" && atrm "$id"
+        atq | LC_ALL=C sort -k7,7 | awk '!x[$1] {x[$1]=1; print $7$1}' |
+            while read -r job ; do
+                id=${job#?}
+                info=$(at -c "$id")
+                echo "$info" | grep -q "^NT_MESSAGE=" || exit
+                case $job in
+                    =*)
+                        eval "$(echo "$info" | grep "^NT_PIDFILE=")"
+                        read -r PID <"$NT_PIDFILE"
+                        /usr/bin/kill -- "-$PID"
+                        rm -f "$NT_PIDFILE"
+                        ;;
+                    *)
+                        atrm "$id"
+                        if pidline=$(echo "$info" | grep "^NT_PIDFILE=") ; then
+                            eval "$pidline"
+                            rm -f "$NT_PIDFILE"
+                        fi
+                        ;;
+                esac
         done
         ;;
     *)
-        atrm "$@"
+        info=$(at -c "$1" 2>/dev/null) || { echo "ntrm: invalid id"; exit ;}
+        echo "$info" | grep -q "^NT_MESSAGE=" || { echo "ntrm: invalid id"; exit ;}
+        atrm "$1"
+        if pidline=$(echo "$info" | grep "^NT_PIDFILE=") ; then
+            eval "$pidline"
+            rm -f "$NT_PIDFILE"
+        fi
         ;;
 esac
