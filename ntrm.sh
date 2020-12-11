c68d42690352b6dfd868d96bfb5477fb0f0f357a
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-11 22:58:52 +0530

    Fixed bugs in ntrm

--- ntrm.sh
+++ ntrm.sh
@@ -2,7 +2,7 @@
 [ "$#" -eq 0 ] && { echo "Usage: ntrm -a|<id>"; exit ;}
 case $1 in
     -a)
-        printf "Remove all pending notification alarms (y/N):"
+        printf "Remove all pending notification alarms (y/N): "
         read -r confirm
         [ "$confirm" != y ] && [ "$confirm" != Y ] && exit
         at -l | LC_ALL=C sort -k7,7 | awk '!x[$1] {x[$1]=1; print $7$1}' |
@@ -13,9 +13,7 @@ case $1 in
                 case $job in
                     =*)
                         eval "$(echo "$info" | grep "^NT_PIDFILE=")"
-                        read -r PID <"$NT_PIDFILE"
-                        kill "$PID"
-                        pkill -P "$PID"
+                        read -r PID <"$NT_PIDFILE" && kill "$PID" $(pgrep -P "$PID")
                         rm -f "$NT_PIDFILE"
                         ;;
                     *)
@@ -31,10 +29,18 @@ case $1 in
     *)
         info=$(at -c "$1" 2>/dev/null) || { echo "ntrm: invalid id"; exit ;}
         echo "$info" | grep -q "^NT_MESSAGE=" || { echo "ntrm: invalid id"; exit ;}
-        at -r "$1"
-        if pidline=$(echo "$info" | grep "^NT_PIDFILE=") ; then
-            eval "$pidline"
-            rm -f "$NT_PIDFILE"
+        if at -l | grep -q "^$1.* =" ; then
+            if pidline=$(echo "$info" | grep "^NT_PIDFILE=") ; then
+                eval "$pidline"
+                read -r PID <"$NT_PIDFILE" && kill "$PID" $(pgrep -P "$PID")
+                rm -f "$NT_PIDFILE"
+            fi
+        else
+            at -r "$1"
+            if pidline=$(echo "$info" | grep "^NT_PIDFILE=") ; then
+                eval "$pidline"
+                rm -f "$NT_PIDFILE"
+            fi
         fi
         ;;
 esac
