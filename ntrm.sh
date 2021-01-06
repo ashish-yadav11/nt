5c62d060001eeb45469c4e1ef6de06b21f21cdf4
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-07 03:59:14 +0530

    Optimized ntrm.sh

--- ntrm.sh
+++ ntrm.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 [ "$#" -eq 0 ] && { echo "Usage: ntrm -a|<id>"; exit ;}
 
-remove_running() {
-    pidfile=$(echo "$info" | awk -F'>' '$1=="echo \"$$\" " {print $2; exit}')
-    read -r PID 2>/dev/null <"$pidfile" && kill "$PID" $(pgrep -P "$PID")
-    rm -f "$pidfile"
-}
-
-remove_pending() {
-    at -r "$id"
-    rm -f "$(echo "$info" | awk -F'>' '$1=="echo \"$$\" " {print $2; exit}')"
-}
-
 case $1 in
     -a)
         printf "Remove all pending notification alarms? [y/N]: "
@@ -21,26 +10,40 @@ case $1 in
         at -l | LC_ALL=C sort -k7,7 | awk '!x[$1] {x[$1]=1; print $7$1}' |
             while read -r job ; do
                 id=${job#?}
-                info=$(at -c "$id")
-                echo "$info" | grep -qm1 "^NT_MESSAGE=" || continue
+                pidfile=$(
+                    at -c "$id" | awk -F'>' '
+                        /^NT_MESSAGE=/ {e=1}
+                        $1=="echo \"$$\" " {print $2}
+                        END {exit !e}
+                    '
+                ) || continue
                 case $job in
                     =*)
-                        remove_running
+                        read -r PID 2>/dev/null <"$pidfile" && kill "$PID" $(pgrep -P "$PID")
+                        rm -f "$pidfile"
                         ;;
                     *)
-                        remove_pending
+                        at -r "$id"
+                        rm -f "$pidfile"
                         ;;
                 esac
         done
         ;;
     *)
         id=$1
-        info=$(at -c "$id" 2>/dev/null) || { echo "ntrm: invalid id"; exit ;}
-        echo "$info" | grep -qm1 "^NT_MESSAGE=" || { echo "ntrm: invalid id"; exit ;}
+        pidfile=$(
+            { at -c "$id" 2>/dev/null || { echo "ntrm: invalid id"; exit ;} ;} |
+                awk -F'>' '
+                    /^NT_MESSAGE=/ {e=1}
+                    $1=="echo \"$$\" " {print $2}
+                    END {exit !e}
+                '
+        ) || { echo "ntrm: invalid id"; exit ;}
         if at -l | awk -v id="$id" '$1==id {exit $7!="="}' ; then
-            remove_running
+            read -r PID 2>/dev/null <"$pidfile" && kill "$PID" $(pgrep -P "$PID")
+            rm -f "$pidfile"
         else
-            remove_pending
+            at -r "$id"
+            rm -f "$pidfile"
         fi
-        ;;
 esac
