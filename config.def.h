8deb54e5c4f17124739e28265db3d609984c64bc
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-06 19:17:05 +0530

    Add -- at the end of notify command

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* command used to send notifications */
-#define NOTIFY                          "notify-send -t 0"
+#define NOTIFY                          "notify-send -t 0 --"
 
 /* prompt when asking for notification message */
 #define NTMESSAGEPROMPT                 "Notification Message> "
