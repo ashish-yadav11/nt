33c8a1dbbb3d488c2a14ad4c650864666bde3528
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-05-10 12:45:06 +0530

    Consistent exit codes - ntrm.sh

--- ntrm.sh
+++ ntrm.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-[ "$#" -eq 0 ] && { echo "Usage: ntrm -a|<id>"; exit ;}
+[ "$#" -eq 0 ] && { echo "Usage: ntrm -a|<id>"; exit 2 ;}
 
 remove_by_id() {
     pidfile=$(
-        { at -c "$1" 2>/dev/null || { echo "ntrm: invalid id" >&2; exit ;} ;} |
+        { at -c "$1" 2>/dev/null || { echo "ntrm: invalid id" >&2; exit 1 ;} ;} |
             awk -F'>' '
                 /^NT_MESSAGE=/ {e=1}
                 $1=="echo \"$$\" " {print $2; exit}
                 END {exit !e}
             '
-    ) || { echo "ntrm: invalid id" >&2; exit ;}
+    ) || { echo "ntrm: invalid id" >&2; exit 1 ;}
 
     if at -l | awk -v id="$1" '$1==id && $7=="=" {e=1; exit}; END {exit !e}' ; then
         read -r PID 2>/dev/null <"$pidfile" && kill "$PID" $(pgrep -P "$PID")
@@ -41,7 +41,7 @@ case $1 in
     -a)
         printf "Remove all pending notification alarms? [y/N]: "
         read -r confirm
-        [ "$confirm" != y ] && [ "$confirm" != Y ] && exit
+        [ "$confirm" != y ] && [ "$confirm" != Y ] && exit 0
 
         at -l | LC_ALL=C sort -k7,7 | awk 'BEGIN {id=-1}; id!=$1 {print $7$1}' |
             while IFS='' read -r job ; do
@@ -51,13 +51,13 @@ case $1 in
     -l)
         at -l | LC_ALL=C sort -r | awk 'BEGIN {id=-1}; id!=$1 {print $7$1}' |
             while IFS='' read -r job ; do
-                remove_by_job "$job" && exit
+                remove_by_job "$job" && exit 0
             done
         ;;
     -n)
         at -l | LC_ALL=C sort -k6,6 -k3,3M -k4,4 -k5,5 | awk 'BEGIN {id=-1}; id!=$1 {print $7$1}' |
             while IFS='' read -r job ; do
-                remove_by_job "$job" && exit
+                remove_by_job "$job" && exit 0
             done
         ;;
     *)
