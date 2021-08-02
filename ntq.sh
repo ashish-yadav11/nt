e89c15d0d28616d39cf5a6dc821d73056f154d86
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-02 15:43:53 +0530

    Use single quotes with escape sequences

--- ntq.sh
+++ ntq.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cid="\033[32m"
-ctm="\033[33m"
-cur="\033[36m"
-cdf="\033[0m"
+cid='\033[32m'
+ctm='\033[33m'
+cur='\033[36m'
+cdf='\033[0m'
 
 at -l | LC_ALL=C sort -k6,6 -k3,3M -k4,5 |
     awk 'BEGIN {id=-1}; id!=$1 {printf "%s,%s %s %2d %s %4d,%s\n",$1,$2,$3,$4,$5,$6,$8; id=$1}' |
